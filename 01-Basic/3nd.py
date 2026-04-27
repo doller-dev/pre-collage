eeb4c8061f171cadb1746ad07cb0633fedf044e1
Nikhil Kumar <nikhilsecure.dev@gmail.com>
2026-04-27 16:09:52 +0530

    Update 3nd.py

--- 01-Basic/3nd.py
+++ 01-Basic/3nd.py
@@ -2,25 +2,14 @@
 print("welcome to Nikhil practical normal calculator.")
 a=int(input("Enter your First Number "))
 b=int(input("Enter your Second Number "))
-d=int(input("Enter your third number "))
-c=a+b+d
+c=a+b
 print("addition : ",c)
-c=a-b-d
+c=a-b
 print("substraction : ",c)
-c=a*b*d
+c=a*b
 print("Multiplication : ",c)
 c=a//b
 print("Division : ",c)
-c=a*a
-print("square of first Number : ",c)
-c=b*b
-print("square of Second Number : ",c)
-c=d*d
-print("square of Third Number : ",c)
-c=a+b-10
-print("after addition 10 is subtracted : ",c)
-c=a+b+100
-print("after addition 100 is added : ",c)
 
 print(type(c))
 
