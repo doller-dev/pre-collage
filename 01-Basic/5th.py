87e4317d17b1eef2cfc80adf78baca3cf3c8dcd1
Nikhil Kumar <nikhilsecure.dev@gmail.com>
2026-04-28 21:27:56 +0530

    Add files via upload

---
+++ 01-Basic/5th.py
@@ -0,0 +1,8 @@
+# welcome to Nikhil adult determined.py file.
+print("check you are adult or not")
+a=input("Enter your Name ")
+b=int(input("enter your age "))
+print("your name is : ",a)
+print("your age is : ",b)
+c=b>18
+print("your are adult : ",c)
