a45cf386af547d24a5b7868d65b094595a1bd972
Nikhil Kumar <nikhilsecure.dev@gmail.com>
2026-04-29 12:00:32 +0530

    Add files via upload

---
+++ 01-Basic/3rd.py
@@ -0,0 +1,32 @@
+# welcome to Nikhil practical normal calculator.py file.
+print("welcome to Nikhil practical normal calculator.")
+a=int(input("Enter your First Number "))
+b=int(input("Enter your Second Number "))
+d=int(input("Enter your third number "))
+c=a+b+d
+print("addition : ",c)
+c=a-b-d
+print("substraction : ",c)
+c=a*b*d
+print("Multiplication : ",c)
+c=a//b
+print("Division : ",c)
+c=a*a
+print("square of first Number : ",c)
+c=b*b
+print("square of Second Number : ",c)
+c=d*d
+print("square of Third Number : ",c)
+c=a+b-10
+print("after addition 10 is subtracted : ",c)
+c=a+b+100
+print("after addition 100 is added : ",c)
+
+print(type(c))
+
+
+
+
+
+
+
