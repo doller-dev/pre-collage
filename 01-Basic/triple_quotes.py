a1ee8195341abd94101b42e5c2e2ffbf047d3909
Nikhil Kumar <nikhilsecure.dev@gmail.com>
2026-05-01 20:20:16 +0530

    Update triple_quotes.py

--- 01-Basic/triple_quotes.py
+++ 01-Basic/triple_quotes.py
@@ -1,12 +1,12 @@
 # using triple quotes to write the code
 _a = int(input("Enter your First Number: "))
-#_b = int(input("enter your second Number")
 _b = int(input("""Enter 
 your second
 Number """))
 _c = _a+_b
-#print("additon is:" ,_c)
+#_b = int(input("enter your second Number")
+
 print(""" additon
         of two number : """ ,_c)
 print(type(_c))
- 
+#print("additon is:" ,_c)
