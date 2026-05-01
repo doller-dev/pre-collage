ff136fc961af092372b1d9954564a5c77f73811a
Nikhil Kumar <nikhilsecure.dev@gmail.com>
2026-05-01 20:18:09 +0530

    Add files via upload

---
+++ 01-Basic/triple_quotes.py
@@ -0,0 +1,12 @@
+# using triple quotes to write the code
+_a = int(input("Enter your First Number: "))
+#_b = int(input("enter your second Number")
+_b = int(input("""Enter 
+your second
+Number """))
+_c = _a+_b
+#print("additon is:" ,_c)
+print(""" additon
+        of two number : """ ,_c)
+print(type(_c))
+ 
