45727755dd720b825ff4fd6980965d6a6e75b1fa
Nikhil Kumar <nikhilsecure.dev@gmail.com>
2026-05-03 10:36:33 +0530

    Add files via upload

---
+++ 01-Basic/single_line_input.py
@@ -0,0 +1,15 @@
+# writing a code for single line input in int/float format.
+
+print('enter your two number with common such as 3,4  65,34 ') 
+a,b = map(int,input("Enter Two number ").split(","))
+c=a+b
+print("additon of two No. is " ,c)
+
+# for float value change int to float like that
+#  a,b = map(float,input("Enter two number ").split(","))
+
+d=a-b
+print("substraction of Two No." ,d)
+
+print(type(c))
+print(type(d))
