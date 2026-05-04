c0a8f439f4fd72ebccccbc7377aea91b820df2da
Nikhil Kumar <nikhilsecure.dev@gmail.com>
2026-05-04 20:11:14 +0530

    Add files via upload

---
+++ 01-Basic/Arithmatical_operator.py
@@ -0,0 +1,77 @@
+# Write a code using all basic Arthmatical operator.
+# Auther :- (Code Name: "Dollar")                                       Date:- 03/05/2026
+
+# for additon(+)
+print (50+49)
+print("Nikhil + Kumar")
+# print("Data" + 10")  it show errors
+
+
+# For Substraction(-)
+print(78-34)
+print("Nikhil - Kumar")
+print ("Nikhil - 10")  # it does'nt show error
+
+
+# For Multiplication(*)
+print(5*3)
+print("Nikhil * 5 ")
+
+
+# For Power(**)
+print (7**2)
+print(7**3)
+print( 35**2)
+print(25 ** 3)
+print(25 ** 4)
+print(25 **25)
+
+# For mod(%) used for finding remainder
+a=15%2
+print(a)
+
+ab=10%2
+print(ab)
+
+abc=123%3
+print(abc)
+
+a1=35664333464%3234
+print(a1)
+
+
+
+# For divison(/) and floor divison(//)
+
+b= 15/2
+print(b)
+
+b1=14/2
+print(b1)
+b2 = 2563434/2
+print(b2)
+ba= 364646366666643636/3634
+print(ba)
+
+
+# for floor divison(//)
+
+b= 15/2
+print(b)
+
+b1=14//2
+print(b1)
+b2 = 2563434//2
+print(b2)
+ba= 364646366666643636//3634
+print(ba)
+
+c1= 18/2
+print(c1)
+c2= 18.0/2
+print(c2)
+c3= 18/2.0
+print(c3)
+c4=18.0/2.0
+print(c4)
+# output come in floor float for c1,c2,c3,c4
