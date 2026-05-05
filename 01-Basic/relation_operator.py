5919136150c7ba32b515358dfd0b949437174942
Nikhil Kumar <nikhilsecure.dev@gmail.com>
2026-05-05 17:07:42 +0530

    Add files via upload

---
+++ 01-Basic/relation_operator.py
@@ -0,0 +1,100 @@
+# # write a code with basic Relational operator.
+# Auther:-                         Date:-
+
+
+# For Greator than(>)
+a1=15>3
+print(a1)
+a2= 2565>34534                        # Output come in the format of TRUE or FALSE.
+print(a2)
+print("  End of greator than")
+
+
+# For less-than(<)
+b1= 3856<34
+print(b1)
+b2= 443<3536
+print(b2)
+print("  End of less than ")
+
+
+# For greator than equal to (>=)
+c1= 25>= 34
+print(c1)
+c2= 12>=12
+print(c2)
+c3= 2465>=533
+print(c3)
+print(" End of greator than equal to")
+
+
+# For  less than equal to (<=)
+d1= 25<=34
+print(d1)
+d2= 12<=12
+print(d2)
+d3= 2465<=533
+print(d3)
+d4 = 3565<=346
+print(d4)
+print(" End of less than equal to")
+
+
+# For  equal to equal to(==)
+print(15==15)
+print(23==35)
+
+E1= 265==354
+print(E1)
+E2= 55555==55555
+print(E2)
+print("  End of equal to equal to")
+
+
+# For not equal to (!=)
+print(15!=15)
+print(23!=35)
+
+F1= 265!=354
+print(F1)
+F2= 55555!=55555
+print(F2)
+F3= 356!=365
+print(F3)
+print("  End of Not equal to ")
+
+
+# mixing of all 
+print(15>34>1)
+print(15>34>0)
+print(15>34>43)
+
+a= 12
+b= 34
+c= 64
+print(a>b>c)
+print(a<b<c)
+print(a==b==c)
+print(a!=b!=c)
+print(" End of mixing a,b,c")
+
+aa=33
+bb= 63
+cc= 1
+print(a>b>c)
+print(a<b<c)
+print(a==b==c)
+print(a!=b!=c)
+print(" End of mixing aa,bb,cc")
+
+
+aaa=33
+bbb= 63
+ccc= 1
+print(aaa<bbb==ccc)
+print(aaa<bbb!=ccc)
+print(aaa>bbb==cccc)
+print(aaa>bbb!=ccc)
+print("End of mixing aaa,bbb,ccc")
+
+
