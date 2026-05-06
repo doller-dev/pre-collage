b542552582c3759e0394f45d87667a428c27ccbd
Nikhil Kumar <nikhilsecure.dev@gmail.com>
2026-05-06 16:15:12 +0530

    Update str_format.py

--- 01-Basic/str_format.py
+++ 01-Basic/str_format.py
@@ -2,11 +2,11 @@
 # Auther :- (Code Name: "Doller")                                            Date:- 06/05/2026
 
 Name=input("Enter your Name ")
-Roll_NO= input("Enter your Roll No. ")
+Roll_no= input("Enter your Roll No. ")
 Age = int(input("Enter your age "))
 
 Adult= Age>18
 
 str="WELCOME TO THE SMALL CODE OF WORLD \n Your Name is {}\n Your current Roll No. is {}\n Ypur current age is {}\n Your are become adult {}"
-print(str.format(Name, Roll_NO, Age, Adult))
+print(str.format(Name, Roll_no, Age, Adult))
 
