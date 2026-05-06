cf55c615cbe935f915d938816531852625e27731
Nikhil Kumar <nikhilsecure.dev@gmail.com>
2026-05-06 16:13:13 +0530

    Update str_format.py

--- 01-Basic/str_format.py
+++ 01-Basic/str_format.py
@@ -1,5 +1,5 @@
 # Write a code using Str.format with varible input value.
-# Auther :- (Code Name: "Doller")                                            Date:- 03/05/2026
+# Auther :- (Code Name: "Doller")                                            Date:- 06/05/2026
 
 Name=input("Enter your Name ")
 Roll_NO= input("Enter your Roll No. ")
@@ -9,3 +9,4 @@ Adult= Age>18
 
 str="WELCOME TO THE SMALL CODE OF DOLLER WORLD \n Your Name is {}\n Your current Roll No. is {}\n Ypur current age is {}\n Your are become adult {}"
 print(str.format(Name, Roll_NO, Age, Adult))
+
