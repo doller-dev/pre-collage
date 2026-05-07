39019a88ca7419dbf3b4494ed137f2f58e27f825
Nikhil Kumar <nikhilsecure.dev@gmail.com>
2026-05-07 21:06:47 +0530

    Fix formatting issues in used_format_string.py

--- 01-Basic/used_format_sting.py
+++ 01-Basic/used_format_sting.py
@@ -1,13 +1,1 @@
 # Write a code with format string.
-#  Author :- (Code Name: "Doller")             Date:- 07-may-2026                       
-
-a=int(input("Enter your first Number "))
-b=int(input("Enter your second Number "))
-# a,b=map(int,input("Enter your Two Number").split(","))
-c=a+b
-# Used %d and %i for int
-print("Additon of %d and %d is %d" %(a,b,c))
-
-d=a-b
-print("substraction of %i nad %i is %i " %(a,b,d))
-print("valve of %d is " %a)
