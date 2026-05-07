5e8fc58b414c4ff2667aa202309109c279ba51ab
Nikhil Kumar <nikhilsecure.dev@gmail.com>
2026-05-07 21:16:21 +0530

    Add files via upload

---
+++ 01-Basic/used_format_sting.py
@@ -0,0 +1,12 @@
+# write a code with format string.
+
+a=int(input("Enter your first Number "))
+b=int(input("Enter your second Number "))
+# a,b=map(int,input("Enter your Two Number").split(","))
+c=a+b
+# used %d and %i for int
+print("Additon of %d and %d is %d" %(a,b,c))
+
+d=a-b
+print("substraction of %i nad %i is %i" %(a,b,d))
+print("valve of %d is" %a)
