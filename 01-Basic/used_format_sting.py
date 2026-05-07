c4331fe31f374e557e7cbfc82510ffd53cd82e4b
Nikhil Kumar <nikhilsecure.dev@gmail.com>
2026-05-07 21:02:38 +0530

    Update used_format_sting.py

--- 01-Basic/used_format_sting.py
+++ 01-Basic/used_format_sting.py
@@ -1,11 +1,11 @@
-# write a code with format string.
-#  Author :- Code Name: "Doller"             Date:- 07-may-2026                       
+# Write a code with format string.
+#  Author :- (Code Name: "Doller")             Date:- 07-may-2026                       
 
 a=int(input("Enter your first Number "))
 b=int(input("Enter your second Number "))
 # a,b=map(int,input("Enter your Two Number").split(","))
 c=a+b
-# used %d and %i for int
+# Used %d and %i for int
 print("Additon of %d and %d is %d" %(a,b,c))
 
 d=a-b
