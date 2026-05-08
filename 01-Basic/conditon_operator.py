c6b69063b6bad16a3b3986f664059ceb84198488
Nikhil Kumar <nikhilsecure.dev@gmail.com>
2026-05-08 22:02:41 +0530

    Add files via upload

---
+++ 01-Basic/conditon_operator.py
@@ -0,0 +1,56 @@
+# write a code with conditon operator.
+# Author:- (Code Name:         )                                  Date:
+
+
+Number= int(input("enter your Number"))
+# print("Hellow") if Number>=0 else print(" Bye")
+
+print("welcome") if Number>=0 else print("try next time")
+print("Device unlock") if Number==100 else print("best of luck for next time")
+
+
+
+00
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
