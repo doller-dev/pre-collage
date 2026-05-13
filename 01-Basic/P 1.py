cafac0ef4723bba62ffb3830361a87fa5807cd81
Nikhil Kumar <nikhilsecure.dev@gmail.com>
2026-05-13 17:53:33 +0530

    Add files via upload

---
+++ 01-Basic/P 1.py
@@ -0,0 +1,15 @@
+# practics
+a=int(input("enter your first number "))
+b=int(input("enter your second number "))
+
+c=a+b
+print(" Addition of first and second number is : ",c)
+
+c=a*b
+print(" multiplication of first and second number is : ",c)
+
+c=a*a
+print(" Square of first number is : ",c)
+
+c=b*b
+print("Square of second number is : ",c)
